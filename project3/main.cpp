12651fa7554cd04b2766dc3ae041465a14a8d456
avin <umutavin@gmail.com>
2018-12-15 21:47:20 +0300

    Initial Commit

---
+++ project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project3
+//
+//  Created by Umut Avin on 15.12.2018.
+//  Copyright © 2018 avin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
